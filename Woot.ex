5bfbb6ba04dd09be21c435669c53f865f7450834
reindeernix <reindeernix@prozum.dk>
2016-01-02 17:46:27 +0100

    wchar neighbors are now updated with neighbor wchar_ids

--- Woot.ex
+++ Woot.ex
@@ -33,71 +33,65 @@ defmodule Woot do
     {-1, 1}
   end
 
-  @spec wstring_pos(wstring, wchar) :: integer
-  def wstring_pos(ws, wc) do
-    {{id, id_clock},_,_,_,_} = wc
+  @spec wstring_pos(wstring, wchar_id) :: integer
+  def wstring_pos(ws, wid) do
+    {id, id_clock} = wid
     Enum.find_index(ws, fn({{i, clock},_,_,_,_}) -> i == id && clock == id_clock end)
   end
 
   #hacked together
-  @spec insert_wc(wstring, integer, char_alpha, wchar_id) :: wstring
-  def insert_wc(ws, pos, alpha, id) do
-    wc_p = wstring_ith_visible(ws, pos-1)
-    wc_n = wstring_ith_visible(ws, pos+1)
-    wc = {id, true, alpha, wc_p, wc_n}
+  @spec insert_wc(wstring, integer, char_alpha, wchar_id, wchar_id, wchar_id) :: wstring
+  def insert_wc(ws, pos, alpha, id, prev_id, next_id) do
+    #visible neighbors of the wchar to be inserted
+    wc_p_pos = wstring_pos(ws, prev_id)
+    wc_n_pos = wstring_pos(ws, next_id)
 
-    wc_p_pos = wstring_pos(ws, wc_p)
-    wc_n_pos = wstring_pos(ws, wc_n)
+    seq = Enum.slice(ws, (wc_p_pos+1)..(wc_n_pos-1))
+    wc_pos = count_wc_pos(seq, {id, true, alpha, id, id}, 0) + pos
 
-    start_s = Enum.slice(ws, 0..(wc_p_pos-1))
-    end_s = Enum.slice(ws, wc_n_pos..(length(ws)))
-    middle_s = Enum.slice(ws, (wc_p_pos+1)..(wc_n_pos-1))
+    {old_p_id, v1, a1, p1, _} = Enum.at(ws, wc_pos)
+    {old_n_id, v2, a2, _, n2} = Enum.at(ws, wc_pos+1)
 
-    insert_wc!(middle_s, wc)
-    start_s ++ middle_s ++ end_s
+    new_wc_p = {old_p_id, v1, a1, p1, id}
+    new_wc_n = {old_n_id, v2, a2, id, n2}
+
+    List.keyreplace(ws, old_p_id, wc_pos, new_wc_p)
+    List.keyreplace(ws, old_n_id, wc_pos, new_wc_n)
+
+    List.insert_at(ws, wc_pos, {id, true, alpha, old_p_id, old_n_id})
   end
 
-  @spec insert_wc!(wstring, wchar) :: wstring
-  def insert_wc!([x|xs], wc) do
-    if compare_wc_id(wc, x) do
-      [wc|[x|xs]]
+  defp count_wc_pos([x|xs], wc, pos) do
+    if compare_wc_id(x, wc) do
+        count_wc_pos(xs, wc, pos+1)
     end
-
-    [x|insert_wc!(xs, wc)]
+    pos
   end
 
-  def insert_wc!([], wc) do
-    wc
+  defp count_wc_pos([], _, pos) do
+    pos
   end
 
-  #if wc1 is before wc2 then true, else false
+  #if wc1 is after wc2 then false, else true
   @spec compare_wc_id(wchar, wchar) :: boolean
   def compare_wc_id(wc1, wc2) do
-    {{id1, clock1},_,_,_,_} = wc1
-    {{id2, clock2},_,_,_,_} = wc2
+    {id1, clock1} = wchar_get_id(wc1)
+    {id2, clock2} = wchar_get_id(wc2)
 
     if id1 < id2 || (id1 == id2 && clock1 < clock2) do
-      true
+      false
     end
 
-    false
+    true
   end
 
   @spec delete_wc(wstring, wchar) :: wstring
   def delete_wc(ws, wc) do
-    pos = wstring_pos(ws, wc)
+    pos = wstring_pos(ws, wchar_get_id(wc))
     slice1 = Enum.slice(ws, 0..(pos-1))
     slice2 = Enum.slice(ws, (pos+1)..(length(ws)))
     new_wc = hide(wc)
     slice1 ++ new_wc ++ slice2
-  end
-
-  # exclusive subsequence of a wstring
-  @spec wstring_subseq(wstring, integer, integer) :: wstring
-  def wstring_subseq(ws, c, d) do
-    s = c + 1 # start position
-    e = d - 1 # end position
-    Enum.slice(ws, s..e)
   end
 
   @spec wstring_contains(wstring, wchar) :: boolean
@@ -117,5 +111,10 @@ defmodule Woot do
   def wstring_ith_visible(ws, i) do
     ws_ = wstring_value(ws)
     Enum.at(ws_, i)
+  end
+
+  def wchar_get_id(wc) do
+    {{id, clock},_,_,_,_} = wc
+    {id, clock}
   end
 end
